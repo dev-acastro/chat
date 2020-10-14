6734de0a89a02ca06eb348b096f847c9db9d6547
Arturo Castro <armicasdi@gmail.com>
2020-10-14 16:38:01 -0600

    Primer Commit

---
+++ dist/js/ajax.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    var data = JSON.stringify({
+                                "data": {
+                                  "username": "armicasdi@gmail.com",
+                                  "password": "0123Castro"
+                                }
+                              });
+
+      var sessionId;
+
+     $.ajax({
+       type: "POST",
+       url: " http://api.ringbyname.com/auth",
+       data: data,
+       success: function(data){
+        sessionId = data.data.state_id;
+       },
+       dataType: "json",
+       contentType: "application/json"
+     });
+
+
+    $.ajax({
+           type: "GET",
+           url: "http://api.ringbyname.com/sms/conversation",
+           headers: { "X-Session-Id" : sessionId},
+           success: function(data){
+            console.log(data)
+           },
+           dataType: "json",
+           contentType: "application/json"
+         });
+
+
+})
